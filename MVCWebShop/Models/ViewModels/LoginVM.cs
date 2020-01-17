aeac335ad4791e48523fca8c43326aed9919b23f
Your <Ansari_hc@hotmail.com>
2020-01-17 16:04:17 +0100

    Add project files.

---
+++ MVCWebShop/Models/ViewModels/LoginVM.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCWebShop.Models.ViewModels
+{
+    public class LoginVM
+    {
+    }
+}
