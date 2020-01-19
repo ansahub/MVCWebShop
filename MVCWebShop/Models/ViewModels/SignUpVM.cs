618ed7a3f6f4fbb756e318a55272ad8dd5f2bb36
Your <Ansari_hc@hotmail.com>
2020-01-19 15:25:27 +0100

    Created all the Viewmodels for User

--- MVCWebShop/Models/ViewModels/SignUpVM.cs
+++ MVCWebShop/Models/ViewModels/SignUpVM.cs
@@ -6,12 +6,19 @@ using System.Threading.Tasks;
 
 namespace MVCWebShop.Models.ViewModels
 {
-    public class LoginVM
+    public class SignUpVM
     {
+        [Required]
+        public string FirstName { get; set; }
+
+        [Required]
+        public string LastName { get; set; }
+
         [Required]
         public string UserName { get; set; }
 
         [Required]
+        [MinLength(6)]
         [DataType(DataType.Password)]
         public string Password { get; set; }
     }
